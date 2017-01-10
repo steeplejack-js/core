bf35108315d4bb019335367bf3e66f551ed55a0b
Simon Emms <simon@simonemms.com>
2017-01-10 17:11:12 +0000

    Begun the process of moving from typescript to es6

--- exception/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { IException } from "../interfaces/exception";
-export declare abstract class Exception extends Error implements IException {
-    message: string;
-    type: string;
-    constructor(message?: any, ...args: any[]);
-    getDetail(): any;
-    getHttpCode(): number;
-}
