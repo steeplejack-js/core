f3e020501db080cc3e5a28b092c794423ecc3534
Simon Emms <simon@simonemms.com>
2017-05-25 21:31:37 +0100

    Updated 503 to 5xx [CI SKIP]

--- src/exception/fatal.js
+++ src/exception/fatal.js
@@ -3,7 +3,7 @@
  *
  * This is an error that cannot be recovered from. This
  * is likely to be either when a datastore cannot respond
- * or similar. Ultimately, this would return an HTTP 503
+ * or similar. Ultimately, this would return an HTTP 5xx
  * error (or equivalent).
  */
 
