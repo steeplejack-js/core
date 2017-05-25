2d46c63fc8e8874a06ca079c24399ba31780150d
Simon Emms <simon@simonemms.com>
2017-05-25 21:34:26 +0100

    Changed 400 to 4xx [CI SKIP]

--- src/exception/validation/index.js
+++ src/exception/validation/index.js
@@ -3,7 +3,7 @@
  *
  * This is an error that can be recovered from.  Normally, this
  * would be invalid input from the client or similar.  Ultimately,
- * this would return something like an HTTP 400 error.
+ * this would return something like an HTTP 4xx error.
  */
 
 /* Node modules */
